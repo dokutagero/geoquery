3aa22006e9602170e7632da7723320634c14ea89
dokutagero <juanjo.rubio@est.fib.upc.edu>
2015-12-30 12:12:54 +0100

    query_builder trying different approaches. Trashy code yet.

--- geoquery/geoquery.py
+++ geoquery/geoquery.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(0,'../lib/')
 import xmlToDict_functions as xml2dict
+from query_builder import local_tagger
 import dbPediaInterface as dbpi
 import labo_classes
 from labo_functions import saveQueriesToXml
@@ -22,29 +23,30 @@ query_list = []
 # Storing the queries in a list.
 # The structure of each training_dict element is {#id : {tags}}
 for k,v in training_dict.iteritems():
-    print v['QUERY']
+    #print v['QUERY']
     query_list.append(v['QUERY'])
 
 # Data pre-processing
 query_list = [query.lower() for query in query_list]
+dbq = local_tagger(query_list[1:10])
 
 
 # Testing DBPedia accessor
-islocal = []
-for q in query_list:
-    print dbpi.isLocation(q)
-    #generic_location = (dbpi.isLocation(q) | dbpi.isCountry(q) | dbpi.isCity(q) | dbpi.isRegion(q))
-    islocal.append(dbpi.isLocation(q))
-
-islocal_output = ['YES' if elem==True else 'NO' for elem in islocal]
-
-# Output XML file
-i=0
-out_geotags = []
-for q in query_list:
-    element = labo_classes.Query(str(i),q,islocal_output[i],"","","","","")
-    out_geotags.append(element)
-    i+=1
-
-saveQueriesToXml(out_geotags, '../test/miau.xml')
+# islocal = []
+# for q in query_list:
+#     print dbpi.isLocation(q)
+#     #generic_location = (dbpi.isLocation(q) | dbpi.isCountry(q) | dbpi.isCity(q) | dbpi.isRegion(q))
+#     islocal.append(dbpi.isLocation(q))
+#
+# islocal_output = ['YES' if elem==True else 'NO' for elem in islocal]
+#
+# # Output XML file
+# i=0
+# out_geotags = []
+# for q in query_list:
+#     element = labo_classes.Query(str(i),q,islocal_output[i],"","","","","")
+#     out_geotags.append(element)
+#     i+=1
+#
+# saveQueriesToXml(out_geotags, '../test/miau.xml')
 
--- geoquery/query_builder.py
+++ geoquery/query_builder.py
@@ -1,23 +1,56 @@
 import nltk
 
-def local_tagger(query):
+def local_tagger(queries):
 
     #Natural Language Processing With Python chapter 7
-    query_tokens = nltk.word_tokenize(query)
-    query_pos = nltk.pos_tag(query_tokens)
+    queries_tokens = [nltk.word_tokenize(query) for query in queries]
+    queries_pos = [nltk.pos_tag(token) for token in queries_tokens]
+    #query_tokens = nltk.word_tokenize(query)
+    #query_pos = nltk.pos_tag(query_tokens)
 
+    #grammar = r"""
+    #    NP: {<DT>?<JJ>*<NN.*>+<DT>?<JJ>*<NN.*>*}
+    #    """
+
+    # Una o mas preposiciones, determinante opcional y uno o mas nombres.
     grammar = r"""
-        NP: {<DT>?<JJ>*<NN.*>+<DT>?<JJ>*<NN.*>*}
-        """
+            PP: {<IN>+<DT>?<NN>+}
+            """
     cp = nltk.RegexpParser(grammar)
-    result = cp.parse(query_pos)
-    print result
+    result = []
+    for query_pos in queries_pos:
+        result.append(cp.parse(query_pos))
+    #result = cp.parse(query_pos)
+    for r in result:
+        print r
 
-    np_subtrees = list(result.subtrees(filter=lambda x: x.label()=='NP'))
+    print "************************************************************************************ \n\n"
+    #np_subtrees = list(result.subtrees(filter=lambda x: x.label()=='PP'))
+    np_subtrees2 = [list(r.subtrees(filter=lambda x: x.label()=='PP')) for r in result]
+    i=0
+    non_detected = []
+    for r in result:
+        if !(r.subtrees(filter=lambda x: x.label()=='PP')):
+
+            non_detected.append(i)
+
+        i += 1
+    np_subtrees3 = [list(r.subtrees(filter=lambda x: x.label()!='PP')) for r in result]
+
 
     islocal = []
     dbpedia_queries = []
-    for np_subtree in np_subtrees:
-            dbpedia_queries.append([tagged_word[0] for tagged_word in np_subtree.leaves() ])
+    for np_subtrees in np_subtrees2:
+        for np_subtree in np_subtrees:
+                dbpedia_queries.append([tagged_word[0] for tagged_word in np_subtree.leaves() ])
+                print np_subtree
+
+    print "************************************************************************************ \n\n"
+
+
+    for np_subtrees in np_subtrees3:
+        for np_subtree in np_subtrees:
+                dbpedia_queries.append([tagged_word[0] for tagged_word in np_subtree.leaves() ])
+                print np_subtree
 
     return dbpedia_queries
